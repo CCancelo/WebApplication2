62a9fb1de5bf551490c5f02c6e2ba53a848f52f5
cance <cance@DESKTOP-8E3RIC4>
2021-03-31 08:22:55 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Mail()
+        {
+            return View();
+        }
+
+        public ActionResult Stats()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Videos()
+        {
+            return View();
+        }
+
+        public ActionResult Written()
+        {
+            return View();
+        }
+    }
+}
